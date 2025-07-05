de71decfb7530cbd161efbf9b779be070454e49f
Omar <omarbriceno999@gmail.com>
2025-07-04 19:56:01 -0600

    removed footer import in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "./components/Footer";
 import LayoutWrapper from './components/LayoutWrapper';
 
 const geistSans = Geist({
