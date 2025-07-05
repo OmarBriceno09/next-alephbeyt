f0325f930d525895f26a42c3e1b17f4da96ee5c9
Omar <omarbriceno999@gmail.com>
2025-07-04 18:50:07 -0600

    added Sanity Asset interface to Character type for groq fetch

--- types/Character.ts
+++ types/Character.ts
@@ -1,5 +1,10 @@
 import { PortableTextBlock } from "sanity";
 
+interface SanityAsset {
+    _id: string;
+    url: string;
+}
+
 export type Character = {
     _id: string;
     _createdAt: Date;
@@ -7,7 +12,9 @@ export type Character = {
     slug: string;
     order_index: Int16Array;
     char_color: string;
-    modern_char: string;
+    modern_char: {
+        asset?: SanityAsset;
+    };
     latin_char: string;
     transliteral: string;
     extended_summary: PortableTextBlock[];
