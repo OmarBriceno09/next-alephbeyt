580f51839c05bc8b01ca9403f60145892764b1dc
Omar <omarbriceno999@gmail.com>
2025-07-30 11:30:05 -0600

    updating letter schema

--- sanity/schemas/letter-schema.ts
+++ sanity/schemas/letter-schema.ts
@@ -15,8 +15,21 @@ const letter = {
                 accept:'.png'
             },
         },
+        { name: 'name_pronounced', title: 'Name Pronounced', type: 'string' },
+        { name: 'letter_pronounced', title: 'Letter Pronounced', type: 'string' },
         { name: 'transliteration', title: 'Transliteration', type: 'string' },
+        { name: 'sounds_like_text', title: 'Sounds Like (text)', type: 'string'},
+        { 
+            name: 'sounds_like_audio', 
+            title: 'Sounds Like (audio)', 
+            type: 'file',
+            options: {
+                accept: 'audio/*'
+            },
+        },
+        { name: 'num_val', title: 'Numerical Value', type: 'string'},
         { name: 'variants', title: 'Variants', type: 'string' },
+        { name: 'classification', title: 'Classification', type: 'string'},
         { name: 'exp_summary', title: 'Expanded Summary', type: 'array', of: [{type: 'block'}]},
         // Add any other fields you need (e.g., SVG, audio, etc.)
     ],
