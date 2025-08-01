003633323864261c6b7fa9da8735b55087b46e4b
Omar <omarbriceno999@gmail.com>
2025-07-31 18:29:17 -0600

    separating stats into letterStats object, 2nd try

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,7 +1,7 @@
 import script from './script-schema';
 import letter from './letter-schema';
 import presImage from './presImage-schema';
-import letterStats from './letterstats-schema';
+import letterStats from './letterStats-schema';
 
 const schemas = [script, letter, presImage, letterStats];
 
