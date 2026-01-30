5787aabb28bcbacf6a0d00d0ebfe2ab443aed46d
Omar <omarbriceno999@gmail.com>
2026-01-30 13:01:19 -0700

    adding extra fields to script schema for tree view

--- sanity/schemas/script-schema.ts
+++ sanity/schemas/script-schema.ts
@@ -5,7 +5,10 @@ const script = {
     type: 'document',
     fields: [
         { name: 'title', title: 'Script Title', type: 'string' },
+        { name: 'is_node', title: 'Is Node?', type: 'boolean'},
         { name: 'order_index', title:'Order Index', type:'number',},
+        { name: 'age_pos', title:'Age Pos', type:'number',},
+        { name: 'points_to', title:'Points To', type:'string', description: 'int sequence where script/node points to'},
         { name: 'font', title: 'Font', type: 'string' },
         { name: 'left_to_right', title: 'Left to Right?', type: 'boolean'},
         { name: 'array_setup', title: 'Array Setup', type: 'string', description: 'int sequence of letters per row denoted by "," (i.e: "7,8,7")', initialValue:'7,8,7'},
--- types/Script.ts
+++ types/Script.ts
@@ -5,7 +5,10 @@ export type Script = {
     _id: string;
     _createdAt: Date;
     title: string;
+    is_node: boolean;
     order_index: number;
+    age_pos: number;
+    points_to: string;
     font: string;
     left_to_right: boolean;
     array_setup: string;
@@ -17,7 +20,10 @@ export const createEmptyScript = (): Script => ({
     _id: "",
     _createdAt: new Date(),       // “now” instead of epoch
     title: "",
+    is_node: false,
     order_index: -1,
+    age_pos: 0,
+    points_to: "",
     font: "",
     left_to_right: false,
     array_setup: "7,8,7",
