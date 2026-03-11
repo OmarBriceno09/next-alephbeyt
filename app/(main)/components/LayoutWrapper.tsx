d48098e1cddb96bb8eb3a989284799e89a333b81
Omar <omarbriceno999@gmail.com>
2026-03-10 18:33:56 -0600

    reorganizing of hierarchy

--- app/(main)/components/LayoutWrapper.tsx
+++ app/(main)/components/LayoutWrapper.tsx
@@ -1,20 +1,14 @@
 /* This wrapper is to be able to layer more things below the */
-'use client';
-
 //import { usePathname } from 'next/navigation';
 import Footer from './Footer';
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
-  //const pathname = usePathname();
-  const hideFooter = true;//pathname.startsWith('/admin');
 
   return (
     <>
       {children}
 
-      {!hideFooter && (
-        <Footer />
-      )}
+      <Footer />
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,10 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import LayoutWrapper from './components/LayoutWrapper';
+import './globals.css'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "AlephBeyt",
-  description: "Exploring the Genealogy of the World",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <LayoutWrapper>{children}</LayoutWrapper>
-        {/*<div className="min-h-[300vh] bg-red-200">
-          <div className="sticky top-[90vh] bg-blue-500 h-20">
-            STICKY TEST
-          </div>
-        </div>*/}
-
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        {children}
       </body>
     </html>
   );
